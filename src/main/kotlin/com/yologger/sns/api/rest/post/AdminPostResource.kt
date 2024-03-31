88ad0d90214738df215b1c35e7f4327532b9dedb
Paul <paul.kwon@bunjang.co.kr>
2024-03-31 19:21:59 +0900

    chore: add admin, search endpoints

---
+++ src/main/kotlin/com/yologger/sns/api/rest/post/AdminPostResource.kt
@@ -0,0 +1,10 @@
+package com.yologger.sns.api.rest.post
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/post/admin/v1")
+class AdminPostResource {
+
+}
